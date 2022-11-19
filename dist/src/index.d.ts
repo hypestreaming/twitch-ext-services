abf4150af78d65140823639320ac6a640fc22f8b
Gil Megidish <gil@megidish.net>
2022-11-19 21:14:42 +0200

    Added TwitchAuth

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -2,3 +2,4 @@ export { Event } from './event';
 export { Function } from './function';
 export { Jwt } from './jwt';
 export { Response } from './response';
+export { TwitchAuth } from './twitch-auth';
